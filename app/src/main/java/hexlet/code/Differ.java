8c08d96b5498c575a59289387882ed7c31428f3f
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-02-16 15:26:40 +0300

    added changes to Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -25,28 +25,34 @@ public class Differ {
         keysSet.addAll(map2.keySet());
 
         for (String key : keysSet) {
-            Map<String, Object> diff = new LinkedHashMap<>();
-            if (map1.containsKey(key) && !map2.containsKey(key)) {
-                diff.put("key", key);
-                diff.put("oldValue", map1.get(key));
-                diff.put("status", "removed");
-            } else if (!map1.containsKey(key) && map2.containsKey(key)) {
-                diff.put("key", key);
-                diff.put("newValue", map2.get(key));
-                diff.put("status", "added");
-            } else if (!Objects.equals(map1.get(key), map2.get(key))) {
-                diff.put("key", key);
-                diff.put("oldValue", map1.get(key));
-                diff.put("newValue", map2.get(key));
-                diff.put("status", "updated");
-            } else {
-                diff.put("key", key);
-                diff.put("oldValue", map1.get(key));
-                diff.put("status", "unchanged");
-            }
-            differences.add(diff);
+            differences.add(generateDiffEntry(key, map1, map2));
         }
         return differences;
+    }
+
+    private static Map<String, Object> generateDiffEntry(String key, Map<String, Object> map1, Map<String, Object> map2) {
+        Map<String, Object> diff = new LinkedHashMap<>();
+        Object value1 = map1.get(key);
+        Object value2 = map2.get(key);
+
+        diff.put("key", key);
+        if (map1.containsKey(key) && map2.containsKey(key)) {
+            if (!Objects.equals(value1, value2)) {
+                diff.put("oldValue", value1);
+                diff.put("newValue", value2);
+                diff.put("status", "updated");
+            } else {
+                diff.put("oldValue", value1);
+                diff.put("status", "unchanged");
+            }
+        } else if (map1.containsKey(key)) {
+            diff.put("oldValue", value1);
+            diff.put("status", "removed");
+        } else {
+            diff.put("newValue", value2);
+            diff.put("status", "added");
+        }
+        return diff;
     }
 
     public static String generate(String pathfile1, String pathfile2) throws Exception {
