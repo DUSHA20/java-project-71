f361b3dcde5d94f1ea0a9d94c7be9f7bdd3cd59a
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-02-16 15:20:33 +0300

    added changes to JsonFormatter

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -10,29 +10,26 @@ public class JsonFormatter {
 
         for (Map<String, Object> diff : differences) {
             result.append("{\n");
-            appendKeyValuePair(result, "key", diff.get("key"));
-            appendKeyValuePair(result, "oldValue", diff.get("oldValue"));
-            appendKeyValuePair(result, "newValue", diff.get("newValue"));
-            appendKeyValuePair(result, "status", diff.get("status"));
+            result.append("  \"key\" : \"").append(diff.get("key")).append("\",\n");
+
+            if (diff.containsKey("oldValue")) {
+                result.append("  \"oldValue\" : ").append(formatValue(diff.get("oldValue"))).append(",\n");
+            }
+
+            if (diff.containsKey("newValue")) {
+                result.append("  \"newValue\" : ").append(formatValue(diff.get("newValue"))).append(",\n");
+            }
+
+            result.append("  \"status\" : \"").append(diff.get("status")).append("\"\n");
             result.append("},\n");
         }
 
-        removeTrailingComma(result);
+        if (!differences.isEmpty()) {
+            result.deleteCharAt(result.length() - 2);
+        }
+
         result.append("]");
         return result.toString();
-    }
-
-    private static void appendKeyValuePair(StringBuilder builder, String key, Object value) {
-        if (value != null) {
-            builder.append("  \"").append(key).append("\" : ").append(formatValue(value)).append(",\n");
-        }
-    }
-
-    private static void removeTrailingComma(StringBuilder builder) {
-        int length = builder.length();
-        if (length > 2 && builder.charAt(length - 2) == ',') {
-            builder.deleteCharAt(length - 2);
-        }
     }
 
     private static String formatValue(Object value) {
