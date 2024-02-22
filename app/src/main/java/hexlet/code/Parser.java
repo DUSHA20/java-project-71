b55bd5e2ce508d634ebd3d7365c3e43e245cc4e1
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-02-22 16:56:04 +0300

    added changes to the Parser file

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -15,26 +15,30 @@ public class Parser {
     private static final ObjectMapper YAML_MAPPER = new YAMLMapper();
 
     public static Map<String, Object> parseFile(String filePath) throws Exception {
-        Path path = Paths.get(filePath);
-        if (!Files.exists(path)) {
+        Path fullPath = getFullPath(filePath);
+        if (!Files.exists(fullPath)) {
             throw new IllegalArgumentException("File not found: " + filePath);
         }
 
         if (filePath.endsWith(".json")) {
-            return jsonFileToMap(path);
+            return jsonFileToMap(fullPath);
         } else if (filePath.endsWith(".yaml") || filePath.endsWith(".yml")) {
-            return yamlFileToMap(path);
+            return yamlFileToMap(fullPath);
         } else {
             throw new IllegalArgumentException("Unsupported file format: " + filePath);
         }
     }
 
     private static Map<String, Object> jsonFileToMap(Path path) throws Exception {
-        return JSON_MAPPER.readValue(path.toFile(), new TypeReference<>() { });
+        return JSON_MAPPER.readValue(path.toFile(), new TypeReference<>() {});
     }
 
     private static Map<String, Object> yamlFileToMap(Path path) throws Exception {
-        return YAML_MAPPER.readValue(path.toFile(), new TypeReference<>() { });
+        return YAML_MAPPER.readValue(path.toFile(), new TypeReference<>() {});
+    }
+
+    private static Path getFullPath(String filePath) {
+        return Paths.get(filePath).toAbsolutePath().normalize();
     }
 }
 
