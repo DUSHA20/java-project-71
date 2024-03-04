45880b8cfbcedc5d1433a47aff423ebe62c18883
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-03-04 21:14:32 +0300

    The logic has been moved from the App class to the necessary classes: Differ and Formatter. Also removed the old App class and renamed Appcommand to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,11 +2,37 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
-public class App {
-    public static void main(String[] args) {
+import java.util.concurrent.Callable;
 
-        AppCommand appCommand = new AppCommand();
-        CommandLine cmd = new CommandLine(appCommand);
-        cmd.execute(args);
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String filePath1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+
+    @CommandLine.Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
+
+    @Override
+    public Integer call() {
+        try {
+            String formattedResult = Differ.generate(filePath1, filePath2, format);
+            System.out.println(formattedResult);
+            return 0;
+        } catch (Exception e) {
+            System.out.println("Error reading or comparing files: " + e.getMessage());
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
