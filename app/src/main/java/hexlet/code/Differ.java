391e8a7dd66361e308bc71a7c7c67084929ba561
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-03-06 13:09:14 +0300

    changes added to Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,9 +2,11 @@ package hexlet.code;
 
 import java.util.Map;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class Differ {
 
@@ -23,9 +25,38 @@ public class Differ {
         keysSet.addAll(map2.keySet());
 
         for (String key : keysSet) {
-            differences.add(TreeBuilder.generateDiffEntry(key, map1, map2)); // Вызываем метод из TreeBuilder
+            differences.add(generateDiffEntry(key, map1, map2));
         }
         return differences;
+    }
+
+    private static Map<String, Object> generateDiffEntry(
+            String key,
+            Map<String, Object> map1,
+            Map<String, Object> map2
+    ) {
+        Map<String, Object> diff = new LinkedHashMap<>();
+        Object value1 = map1.get(key);
+        Object value2 = map2.get(key);
+
+        diff.put("key", key);
+        if (map1.containsKey(key) && map2.containsKey(key)) {
+            if (!Objects.equals(value1, value2)) {
+                diff.put("oldValue", value1);
+                diff.put("newValue", value2);
+                diff.put("status", "updated");
+            } else {
+                diff.put("oldValue", value1);
+                diff.put("status", "unchanged");
+            }
+        } else if (map1.containsKey(key)) {
+            diff.put("oldValue", value1);
+            diff.put("status", "removed");
+        } else {
+            diff.put("newValue", value2);
+            diff.put("status", "added");
+        }
+        return diff;
     }
 
     public static String generate(String pathfile1, String pathfile2) throws Exception {
