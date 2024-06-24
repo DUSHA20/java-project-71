d4ee216e8fd8baf21eb54e8f6665a8e1f9d88da5
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-06-24 12:31:54 +0300

    JsonFormatter class changed

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -1,41 +1,17 @@
 package hexlet.code.formatters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import java.util.List;
 import java.util.Map;
 
 public class JsonFormatter {
 
-    public static String formatJson(List<Map<String, Object>> differences) {
-        StringBuilder result = new StringBuilder("[\n");
+        public static String formatJson(List<Map<String, Object>> differences) throws Exception {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        for (Map<String, Object> diff : differences) {
-            result.append("{\n");
-            result.append("  \"key\" : \"").append(diff.get("key")).append("\",\n");
-
-            if (diff.containsKey("oldValue")) {
-                result.append("  \"oldValue\" : ").append(formatValue(diff.get("oldValue"))).append(",\n");
-            }
-
-            if (diff.containsKey("newValue")) {
-                result.append("  \"newValue\" : ").append(formatValue(diff.get("newValue"))).append(",\n");
-            }
-
-            result.append("  \"status\" : \"").append(diff.get("status")).append("\"\n");
-            result.append("},\n");
+            return mapper.writeValueAsString(differences);
         }
 
-        if (!differences.isEmpty()) {
-            result.deleteCharAt(result.length() - 2);
-        }
-
-        result.append("]");
-        return result.toString();
-    }
-
-    private static String formatValue(Object value) {
-        if (value instanceof String) {
-            return "\"" + value + "\"";
-        }
-        return String.valueOf(value);
-    }
 }
